2e3f657e4aa4edd023d9c59acf9b3df6dcf54d0e
Lena Kucharczuk <lenakucharczuk@gmail.com>
2022-02-23 22:41:10 +0100

    Clean autogenerated methods

--- src/main/java/ncdc/task/bookapp/infractructure/PersistentBook.java
+++ src/main/java/ncdc/task/bookapp/infractructure/PersistentBook.java
@@ -5,7 +5,6 @@ import ncdc.task.bookapp.domain.Book;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 public final class PersistentBook {
@@ -35,52 +34,5 @@ public final class PersistentBook {
 
     public Book toDomain() {
         return new Book(title, author, isbn);
-    }
-
-    public String title() {
-        return title;
-    }
-
-    public String author() {
-        return author;
-    }
-
-    public String isbn() {
-        return isbn;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (PersistentBook) obj;
-        return Objects.equals(this.title, that.title) &&
-            Objects.equals(this.author, that.author) &&
-            Objects.equals(this.isbn, that.isbn);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, isbn);
-    }
-
-    @Override
-    public String toString() {
-        return "PersistentBook[" +
-            "title=" + title + ", " +
-            "author=" + author + ", " +
-            "isbn=" + isbn + ']';
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
     }
 }
